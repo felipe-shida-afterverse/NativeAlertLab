c22de6f2cfcd5c0a91f2aaf582ef663ea93f8221
Felipe Shida <felipe.shida@MAC-343.local>
2020-09-16 14:53:47 -0300

    native toast and dialog

---
+++ Assets/Plugins/Android/ToastSender.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ToastSender : MonoBehaviour
+{
+    private const string UNITY_PLAYER_CLASS_PACKAGE = "com.unity3d.player.UnityPlayer";
+    private Button sendToastButton;
+    [SerializeField] string dialogTitle;
+    [SerializeField] string toastString;
+    [SerializeField] string toastLength;
+    
+    AndroidJavaObject currentActivity;
+
+    private void Start()
+    {
+        sendToastButton = GetComponent<Button>();
+        sendToastButton.onClick.AddListener(SendToast);
+    }
+
+    public void SendToast()
+    {
+#if UNITY_ANDROID
+        StartCoroutine(RunToastOnUIThreadAndroid());
+#elif UNITY_IOS
+        StartCoroutine(RuniOSDialog());
+#endif
+    }
+
+    [DllImport("__Internal")]
+    private static extern void ShowAlert(string title, string message);
+
+    private IEnumerator RuniOSDialog()
+    {
+        ShowAlert(dialogTitle, toastString);
+        
+        yield return null;
+    }
+
+    private IEnumerator RunToastOnUIThreadAndroid()
+    {
+        AndroidJavaClass UnityPlayer = new AndroidJavaClass(UNITY_PLAYER_CLASS_PACKAGE); 
+     
+        currentActivity = UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        currentActivity.Call ("runOnUiThread", new AndroidJavaRunnable (ShowToast));
+
+        yield return null;
+    }
+
+    private void ShowToast()
+    {
+        Debug.Log ("Running on UI thread");
+        object[] toastParams = new object[3];
+        
+        toastParams[0] = currentActivity;
+        toastParams[1] = toastString;
+
+        AndroidJavaClass Toast = new AndroidJavaClass("android.widget.Toast");
+        
+        toastParams[2] = Toast.GetStatic<int> (toastLength);
+        
+        AndroidJavaObject toast = Toast.CallStatic<AndroidJavaObject> ("makeText", toastParams);
+
+        toast.Call ("show");
+    }
+}
---
+++ Assets/Plugins/iOS/NativeDialog.mm
@@ -0,0 +1,31 @@
+#import  "UnityAppController.h"
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern UIViewController *UnityGetGLViewController();
+
+@interface iOSPlugin : NSObject
+
+@end
+
+@implementation iOSPlugin
+
++(void) alertView: (NSString*) title addMessage: (NSString*) message
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *defaultAction = [UIAlertAction actionWithTitle: @"OK" style: UIAlertActionStyleDefault handler: nil];
+    
+    [alert addAction: defaultAction];
+    [UnityGetGLViewController() presentViewController:alert animated:YES completion: nil];
+}
+
+@end
+
+extern "C"
+{
+    void ShowAlert(const char* title, const char* message)
+    {
+        [iOSPlugin alertView: [NSString stringWithUTF8String:title] addMessage:[NSString stringWithUTF8String:message]];
+    }
+}
